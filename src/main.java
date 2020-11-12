9e444a68d11c46f9ae63cdb9e6754c441a928008
Zakee Akram <Zakee-Akram@outlook.com>
2020-11-12 12:21:57 +0000

    Base Project Test

--- src/main.java
+++ src/main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        System.out.println("Select option you would like to do:");
+        System.out.println("1) Multiple File Rename");
+        Scanner userIn = new Scanner(System.in);
+        try {
+            int option = userIn.nextInt();
+            switch (option) {
+                case 1:
+                    multiRename();
+                default:
+                    System.out.println("Enter Valid Number");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+    public static void multiRename(){
 
     }
 }
